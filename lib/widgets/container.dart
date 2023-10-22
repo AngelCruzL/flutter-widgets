75deb88b73d964b553acbb733fe0e586ddab7962
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-10-21 21:09:29 -0600

    Add container shadows example

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -5,25 +5,36 @@ class MyContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: const TextStyle(),
-      child: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          alignment: Alignment.bottomCenter,
-          decoration: const BoxDecoration(
-            color: Colors.tealAccent,
-            // borderRadius: BorderRadius.all(Radius.circular(100)),
-            shape: BoxShape.circle,
-          ),
-          padding: const EdgeInsets.symmetric(
-            horizontal: 30,
-            vertical: 50,
-          ),
-          child: const Text(
-            'Ángel Cruz',
-            style: TextStyle(color: Colors.black, fontSize: 20),
+    return Container(
+      color: Colors.white,
+      child: DefaultTextStyle(
+        style: const TextStyle(),
+        child: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              // borderRadius: BorderRadius.all(Radius.circular(100)),
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(.25),
+                  blurRadius: 25,
+                  spreadRadius: 10,
+                  offset: const Offset(0, -15),
+                ),
+              ],
+            ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: 30,
+              vertical: 50,
+            ),
+            child: const Text(
+              'Ángel Cruz',
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
           ),
         ),
       ),
